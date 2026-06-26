f037a45a592c7857521c73abfd60894f84a4313e
GreenCitrus6 <DanielAAad01@gmail.com>
2026-06-26 15:53:43 +0900

    added useEffect to get all posts and log to console

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
+'use client';
+import api from './api/axiosConfig'
+import {useState, useEffect} from 'react';
+
 
 export default function Home() {
+  const [posts, setPosts] = useState();
+  
+  const getPosts = async () => {
+    try {
+      const response = await api.get("/api/v1/blog");
+      console.log(response.data);
+      setPosts(response.data);
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
+  useEffect(() => {
+    getPosts();
+  },[]);
+
   return (
     <>
+      
     </>
   );
 }
