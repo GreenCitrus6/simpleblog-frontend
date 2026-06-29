550dc52eef1a5dbc3b1298f311acd6180510da63
GreenCitrus6 <DanielAAad01@gmail.com>
2026-06-29 15:38:41 +0900

    created new post button

--- app/components/AppNavbar.tsx
+++ app/components/AppNavbar.tsx
@@ -1,10 +1,17 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Button, Toolbar, Typography } from "@mui/material";
 
 export default function AppNavbar() {
     return(
         <AppBar position="fixed">
-            <Toolbar>
+            <Toolbar sx={{ display: 'flex',
+                justifyContent: 'space-between'
+             }}>
                 <Typography>Simple Blog</Typography>
+                <Button>
+                    <Typography sx={{ color:'#FFF' }}>
+                        Create New Post
+                    </Typography>
+                </Button>
             </Toolbar>
         </AppBar>
     );
